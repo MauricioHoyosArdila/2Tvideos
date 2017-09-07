e03ae87587f8e1064d9e7895eda56bc448b5925d
Daniel Rendon Montaño <dani-9911@hotmail.com>
2017-09-07 16:20:23 -0500

    Mongo replica set

--- config/config.js
+++ config/config.js
@@ -1,8 +1,18 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    env = process.env.NODE_ENV || 'replica';
 
 var config = {
+  replica: {
+    baseUrl: "/2tvideos/",
+    root: rootPath,
+    app: {
+      name: '2Tvideos'
+    },
+    port: process.env.PORT || 3001,
+    db: 'mongodb://mongo1:27017,mongo2:27017,mongo3:27017/2Tvideos'
+  },
+
   development: {
     baseUrl: "/2tvideos/",
     root: rootPath,
